4a23165be0f2dfd87398381cd20dbdd94e52365d
walidgzih12 <walidgzih3@gmail.com>
2024-01-25 09:47:58 +0100

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("music", function () {
+    var homeLink = document.querySelector('.hhh');
+
+    homeLink.addEventListener("click", function () {
+        window.location.href = "ii.html";
+    });
+});
